a3116e89327782b389354a367cb039344e117463
Zellat Abdelkhalek <34510420+AbdelkhalekESI@users.noreply.github.com>
2018-07-15 20:39:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express") ,
 app = express() , 
-port = process.env.port || 3000  , 
+port = process.env.PORT || 3000  , 
 bodyParser = require("body-parser") ; 
 
 app.use(bodyParser.json()) ;
